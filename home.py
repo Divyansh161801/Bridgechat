de28adfcf0edc8e2f2094be3ec9e08e638368b8c
Divyansh161801 <divyanshkushwaha161801@gmail.com>
2024-12-31 17:19:26 +0530

    Update home.py adding email in user

--- home.py
+++ home.py
@@ -72,6 +72,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
 
 
 @app.route('/register', methods=['GET', 'POST'])
