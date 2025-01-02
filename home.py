2bf38bfd85bed184d9c2acbeadb5d49a5dd5acfd
Divyansh161801 <divyanshkushwaha161801@gmail.com>
2025-01-02 22:56:36 +0530

    Update home.py

--- home.py
+++ home.py
@@ -165,10 +165,11 @@ def logout():
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/chatroom')
+@app.route('/chatroom',method=['get','post'])
 @login_required
 def chatroom():
-    return render_template('chatroom.html')
+    if request.method == 'post'
+        return render_template('chatroom.html')
 
 # Google Drive API setup
 def get_drive_service():
